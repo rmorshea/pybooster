e84fb4ce56130b019b564e6a65d9c5bc8bbd5e27
Ryan Morshead <ryan.morshead@gmail.com>
2024-10-08 02:10:21 -0600

    change version

--- src/pybooster/__init__.py
+++ src/pybooster/__init__.py
@@ -3,7 +3,7 @@ from pybooster import provider
 from pybooster.shared import shared
 from pybooster.types import required
 
-__version__ = "0.0.5"
+__version__ = "0.0.1"
 
 __all__ = (
     "injector",
